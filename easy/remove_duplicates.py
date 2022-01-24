3ba89450335fccc1ba7ee08fe2d8b288760fb052
win-study <win-study@Winnies-AS-Air.local>
2022-01-23 16:23:29 -0800

    remove duplicate by checking whether it is different from the previous index :honeybee:

--- easy/remove_duplicates.py
+++ easy/remove_duplicates.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+"""
 def removeDuplicates(nums: List[int]) -> int:
     unique_indexes = {}
     # Add unique number (key) to dict
@@ -15,5 +16,16 @@ def removeDuplicates(nums: List[int]) -> int:
         count += 1
     # Return number of unique numbers in the first partial of the list
     return len(unique_indexes.keys())
+"""
+def removeDuplicates(nums: List[int]) -> int:
+    # First index is unique, starting from the second index
+    new_len = 1
+    for index in range(1, len(nums)):
+        # Add number when it is not duplicate
+        if nums[index-1] != nums[index]:
+            nums[new_len] = nums[index]
+            new_len += 1
+    return new_len
+
 
 print(removeDuplicates([1,1,2]))
