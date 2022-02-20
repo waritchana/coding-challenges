bfa1c39414a9a3ccf380a5c27b09108710024215
win-study <win-study@Winnies-AS-Air.local>
2022-02-20 12:17:51 -0800

    remove all occurrences value in the same array :honeybee:

---
+++ easy/remove_element.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def removeElement(nums: List[int], val: int) -> int:
+    replacing_index = 0
+    for i in range(0, len(nums)):
+        if nums[i] != val:
+            nums[replacing_index] = nums[i]
+            replacing_index += 1
+    return replacing_index
+
+
+nums = [0,1,2,2,3,0,4,2]
+length = removeElement(nums, 2)
+# Expect [0, 1, 3, 0, 4]
+print(nums[0:length])
