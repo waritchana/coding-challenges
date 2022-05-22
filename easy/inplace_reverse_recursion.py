52d0023c9d345bf162a371bf81518d7257f86de9
win-study <win-study@Air-2020.local>
2022-05-22 15:32:04 -0700

    reverse string recursively :honeybee:

---
+++ easy/inplace_reverse_recursion.py
@@ -0,0 +1,25 @@
+from typing import List
+
+def helper(start_index, end_index, s):
+    if start_index >= end_index:
+        return
+    # Swap
+    tmp = s[start_index]
+    s[start_index] = s[end_index]
+    s[end_index] = tmp
+    # Move on to do next index
+    helper(start_index+1, end_index-1, s)
+    return s
+
+
+def reverseString(s: List[str]) -> None:
+    """
+    Do not return anything, modify s in-place instead.
+    """
+    # Recursive way
+    s = helper(0, len(s)-1, s)
+    print(s)
+
+
+s = ["h","e","l","l","o"]
+reverseString(s)
