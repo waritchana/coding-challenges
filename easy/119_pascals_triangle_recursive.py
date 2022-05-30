f9d03c627fea64ee132ea2a32d93ed39631b414f
win-study <win-study@Air-2020.local>
2022-05-30 15:57:08 -0700

    minor fix iterative number - row 5 means index 5 :honeybee:

--- easy/119_pascals_triangle_recursive.py
+++ easy/119_pascals_triangle_recursive.py
@@ -2,20 +2,20 @@ from typing import List
 
 
 def getRow(numRows: int) -> List[List[int]]:
-    if numRows == 1:
+    if numRows == 0:
         return [1]
-    elif numRows == 2:
+    elif numRows == 1:
         return [1, 1]
     else:
         # Pre-created a row with all 1s
-        row = [1] * numRows
+        row = [1] * (numRows+1)
         # Recursively gets previous row
         prev_row = getRow(numRows-1)
         # Only calculate value for middle blocks
-        for i in range(1, numRows-1):
+        for i in range(1, numRows):
             row[i] = prev_row[i-1] + prev_row[i]
         return row
 
 
-print(getRow(5))
+print(getRow(3))
 print(getRow(1))
