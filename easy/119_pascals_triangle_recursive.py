69319f24f40a01af486216ae45e2f1bcb3adfc8a
win-study <win-study@Air-2020.local>
2022-05-30 15:44:47 -0700

    solve pascal triangle using recursive to get previous rows :honeybee:

---
+++ easy/119_pascals_triangle_recursive.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+def getRow(numRows: int) -> List[List[int]]:
+    if numRows == 1:
+        return [1]
+    elif numRows == 2:
+        return [1, 1]
+    else:
+        # Pre-created a row with all 1s
+        row = [1] * numRows
+        # Recursively gets previous row
+        prev_row = getRow(numRows-1)
+        # Only calculate value for middle blocks
+        for i in range(1, numRows-1):
+            row[i] = prev_row[i-1] + prev_row[i]
+        return row
+
+
+print(getRow(5))
+print(getRow(1))
