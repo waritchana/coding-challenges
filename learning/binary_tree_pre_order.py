e8096b62580756aabe483b4f54b1b3a2b1b73ae9
win-study <win-study@Air-2020.local>
2022-10-10 22:47:04 -0700

    Add solving preorder binary traversal using stack :honeybee:

--- learning/binary_tree_pre_order.py
+++ learning/binary_tree_pre_order.py
@@ -8,6 +8,26 @@
 
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+
+        stack = [root]
+        output_tree = []
+
+        while stack:
+            # Removes the element in LIFO
+            current_node = stack.pop()
+            if current_node is not None:
+                output_tree.append(current_node.val)
+                # Add right node first because it will get pop last
+                if current_node.right is not None:
+                    stack.append(current_node.right)
+                if current_node.left is not None:
+                    stack.append(current_node.left)
+        return output_tree
+
+    """
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         # Root first -> traverse to left subtree -> traverse to right subtree
         result = []
         if root:
@@ -15,6 +35,7 @@ class Solution:
             result.extend(self.preorderTraversal(root.left))
             result.extend(self.preorderTraversal(root.right))
         return result
+    """
 
 
 # This part will not work running, just to keep possible inputs for practice
