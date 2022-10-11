0bb02bb4dbbaa717fcb66e5e9b714d15da45a7ae
win-study <win-study@Air-2020.local>
2022-10-10 22:46:45 -0700

    Add solving inorder binary traversal using stack :honeybee:

--- learning/binary_tree_in_order.py
+++ learning/binary_tree_in_order.py
@@ -8,6 +8,27 @@
 
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return None
+
+        stack = []
+        output_tree = []
+
+        while stack or root is not None:
+            # Add nodes to stack until reaching the most left node
+            # of current node (root)
+            while root is not None:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            output_tree.append(root.val)
+            # Go right from the node, upper level will get pop
+            # next round if right is None
+            root = root.right
+        return output_tree
+
+    """
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         # Left subtree -> root -> traverse to right subtree
         result = []
         if root:
@@ -15,6 +36,7 @@ class Solution:
             result.append(root.val)
             result.extend(self.inorderTraversal(root.right))
         return result
+    """
 
 
 # This part will not work running, just to keep possible inputs for practice
