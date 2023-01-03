21204f38118b6c5f21b66830c8adce43c351408b
win-study <win-study@Air-2020.local>
2023-01-02 21:15:45 -0800

    Solve two easy questions using hash table :honeybee:

---
+++ easy/217_contains_duplicate.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashmap = {}
+        for num in nums:
+            if num not in hashmap.keys():
+                hashmap[num] = True
+            else:
+                return True
+        return False
+
+
+nums = [1,2,3,1] # True
+#nums = [1,2,3,4] # False
+#nums = [1,1,1,3,3,4,3,2,4,2] # True
+result = Solution().containsDuplicate(nums)
---
+++ easy/242_is_anagram.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # O(n) from length of s + t
+        hashmap = {}
+        if len(s) != len(t):
+            return False
+        for each in s:
+            if each not in hashmap.keys():
+                hashmap[each] = 1
+            else:
+                hashmap[each] += 1
+        for each in t:
+            if each not in hashmap.keys() or hashmap[each] == 0:
+                return False
+            else:
+                hashmap[each] -= 1
+        return True
+        """
+        O(nlogn) from the sorted function
+        if len(s) != len(t):
+            return False
+        elif sorted(s) == sorted(t):
+            return True
+        else:
+            return False
+        """
+
+
+s = "anagram"
+t = "nagaram"
+#s = "rat", t = "car"
+result = Solution().isAnagram(s, t)
+print("Is it anagram? :", result)
