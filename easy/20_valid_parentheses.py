dc9fe466b25ec12f37733a0248c6b2ba15b1126b
win-study <win-study@Air-2020.local>
2023-02-02 22:51:46 -0800

    Shorten and more strightforward version to solve valid p arentheses - one year ago :honeybee:

---
+++ easy/20_valid_parentheses.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        brakets_table = {
+            ")": "(",
+            "}": "{",
+            "]": "["
+        }
+        for character in s:
+            # If character is the close bracket
+            # check last added character in the stack to match
+            if character in brakets_table.keys():
+                if stack and stack[-1] == brakets_table[character]:
+                    stack.pop()
+                else:
+                    return False
+            # Add open bracket to stack
+            else:
+                stack.append(character)
+        # Return True if stack is empty cause all brackets are closed
+        if not stack:
+            return True
+
+
+result = isValidParentheses("()")
+print(result)
+result = isValidParentheses("}(")
+print(result)
+result = isValidParentheses("()[]{}")
+print(result)
+result = isValidParentheses("(([]{}))")
+print(result)
