eb6c4c31009adb56f42a41cdb293aa93b2b1e605
win-study <win-study@Air-2020.local>
2023-02-05 11:32:35 -0800

    Add number of questions to match with leetcode website, easy to navigate :honeybee:

--- medium/my_pow_iterative.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        result = x
-        for i in range(1, n):
-            result *= x
-        return result
-
-
-print("Result of power of n is", Solution().myPow(2.00000, 10))
