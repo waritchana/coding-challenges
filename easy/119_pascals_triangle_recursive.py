cbec6c9b4905b3a47425fad605e2d1eb10052e25
win-study <win-study@Air-2020.local>
2023-03-19 15:58:36 -0700

    Minor new approach to keep pascal triangle result :honeybee:

--- easy/119_pascals_triangle_recursive.py
+++ easy/119_pascals_triangle_recursive.py
@@ -2,6 +2,7 @@ from typing import List
 
 
 def getRow(numRows: int) -> List[List[int]]:
+    """
     if numRows == 0:
         return [1]
     elif numRows == 1:
@@ -15,6 +16,18 @@ def getRow(numRows: int) -> List[List[int]]:
         for i in range(1, numRows):
             row[i] = prev_row[i-1] + prev_row[i]
         return row
+    """
+    if numRows == 0:
+        return [1]
+    elif numRows == 1:
+        return [1, 1]
+    else:
+        prev_row = self.getRow(numRows-1)
+        row = [1]
+        for i in range(1, numRows):
+            row.append(prev_row[i-1]+prev_row[i])
+        row.append(1)
+    return row
 
 
 print(getRow(3))
