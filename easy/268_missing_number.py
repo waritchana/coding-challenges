bac579ff913b5877d775e1a038ca3a1099434345
win-study <win-study@Air-2020.local>
2023-03-30 23:08:47 -0700

    Find missing number from a list by just add and subtract :honeybee:

---
+++ easy/268_missing_number.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        # The biggest value as for loop does not go to the last value
+        # other numbers get add in the loop
+        result = len(nums)
+        for i in range(len(nums)):
+            result += i
+            result -= nums[i]
+        return result
+
+
+nums = [3,0,1] # 2
+#nums = [0,1] # 2
+#nums = [9,6,4,2,3,5,7,0,1] # 8
+Solution().singleNumber(nums)
