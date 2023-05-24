8a3fccca22b13a3c4a7fa5903d86f24caebe5f68
win-study <win-study@Air-2020.local>
2023-05-23 22:43:55 -0700

    Remove worse solution :honeybee:

--- easy/121_buy_sell_stock.py
+++ easy/121_buy_sell_stock.py
@@ -3,20 +3,6 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        """
-        day_buy = 0
-        day_sell = 1
-        max_profit = 0
-
-        while day_sell < len(prices):
-            if prices[day_sell] > prices[day_buy]:
-                new_profit = prices[day_sell] - prices[day_buy]
-                max_profit = max(new_profit, max_profit)
-            else:
-                day_buy = day_sell
-            day_sell += 1
-        return max_profit
-        """
         # Acts as an unbounded upper value to find minimum value
         # when doing comparison
         min_price = float('inf')
@@ -29,5 +15,5 @@ class Solution:
         return max_profit
 
 
-Solution().maxProfit([7,1,5,3,6,4]) # output = 5
-Solution().maxProfit([7,6,4,3,1]) # output = 0
+print(Solution().maxProfit([7,1,5,3,6,4])) # output = 5
+print(Solution().maxProfit([7,6,4,3,1])) # output = 0
