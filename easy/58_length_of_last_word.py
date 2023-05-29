f3f07e88edb77aedbd6dbde4fae87f0adb723cce
win-study <win-study@Air-2020.local>
2023-05-29 16:59:08 -0700

    Add a solution and minor logic :honeybee:

--- easy/58_length_of_last_word.py
+++ easy/58_length_of_last_word.py
@@ -1,18 +1,25 @@
 def lengthOfLastWord(s: str) -> int:
+    word_len = 0
+    if len(s) == 1 and s[0].isalnum():
+        return 1
+    for i in range(len(s)-1, -1, -1):
+        if s[i].isalnum():
+            word_len += 1
+        elif word_len > 1:
+            return word_len
+    """
     count = 0
-    found = False
     if len(s) == 1 and s[0] != ' ':
         return 1
     # Starting from the last index to index 0
     for i in range(len(s)-1, -1, -1):
         if s[i] != ' ':
             count += 1
-            found = True
         # After getting last word's length and see space again
-        if s[i] == ' ' and found:
+        if s[i] == ' ' and count > 0:
             break
     return count
-
+    """
 
 s = "Hello World" # Expect 5
 s = "   fly me   to   the moon  " # Expect 4
