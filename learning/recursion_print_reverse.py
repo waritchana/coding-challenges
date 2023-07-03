ba05618a0f49e0a1ec52546bec60cbdae3dff333
win-study <win-study@Air-2020.local>
2023-07-02 22:11:58 -0700

    Move print reverse recursively to learning folder :honeybee:

--- learning/recursion_print_reverse.py
+++ learning/recursion_print_reverse.py
@@ -1,3 +1,4 @@
+"""
 def helper(index, s):
     # Base case is when it has reached the end of the string
     if len(s) == 0 or index == len(s):
@@ -5,6 +6,21 @@ def helper(index, s):
     helper(index+1, s)
     # Start printing when base case has met and back
     print(s[index])
+
+def print_reverse(s):
+    helper(0, s)
+
+print_reverse("ABCDEFG")
+"""
+
+def helper(index, s):
+    if len(s) == 0 or index == len(s):
+        return
+    print(index)
+    helper(index+1, s)
+    print(s[index])
+
+
 
 def print_reverse(s):
     helper(0, s)
