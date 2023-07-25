fe64d5c4c21e23f47eb2b3785b4388dfbe4a1cb0
win-study <win-study@Air-2020.local>
2023-07-24 23:05:33 -0700

    Add input linked list and improve comments :honeybee:

--- easy/206_reverse_linked_list_iterative.py
+++ easy/206_reverse_linked_list_iterative.py
@@ -10,12 +10,38 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Iterative
         prev_node = None
-        curr_node = head
-        for curr_node is not None:
-            next_node = curr_node.next
-            curr_node.next = prev_node # Points backward
-            prev_node = curr_node
-            curr_node = next_node
-        head = prev_node # new head is the last item of original node
-        return head
+        current_node = head
+        while current_node:
+            next_node = current_node.next # Next node to work on
+            current_node.next = prev_node # Points backward
+            prev_node = current_node # Current node to be pointed to in the next iteration
+            current_node = next_node # Move to the following node
+        # At the end, prev_node is the new head
+        return prev_node
+
+
+head = ListNode(
+    val=1,
+    next=ListNode(
+        val=2,
+        next=ListNode(
+            val=3,
+            next=ListNode(
+                val=4,
+                next=ListNode(
+                    val=5,
+                    next=None
+                )
+            )
+        )
+    )
+)
+new_head = Solution().reverseList(head)
+print(new_head.val)
+print(new_head.next.val)
+print(new_head.next.next.val)
+print(new_head.next.next.next.val)
+print(new_head.next.next.next.next.val)
+print(new_head.next.next.next.next.next)
--- easy/206_reverse_linked_list_recursive.py
+++ easy/206_reverse_linked_list_recursive.py
@@ -10,15 +10,40 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Base case
-        if (not head) or (not head.next):
+        # Check head empty for an empty input case
+        # Base case when head.next is null
+        if not head or not head.next:
             return head
-        # Reach base case where head = 4 and head.next = 5
-        # reversed_list is 5
-        reversed_list = self.reverseList(head.next)
-        # 4 -> 5 -> 4 cyclic linked list
+        # [1,2,3,4,5] reaching the base case where new_head is 5
+        new_head = self.reverseList(head.next)
+        # 4 -> 5 -> None now 4 -> 5 -> 4 pointing backward
         head.next.next = head
-        # 5 -> 4 -> None
+        # 4 -> None break the cycle
         head.next = None
         # Return 5 and its following nodes
-        return reversed_list
+        return new_head
+
+
+head = ListNode(
+    val=1,
+    next=ListNode(
+        val=2,
+        next=ListNode(
+            val=3,
+            next=ListNode(
+                val=4,
+                next=ListNode(
+                    val=5,
+                    next=None
+                )
+            )
+        )
+    )
+)
+new_head = Solution().reverseList(head)
+print(new_head.val)
+print(new_head.next.val)
+print(new_head.next.next.val)
+print(new_head.next.next.next.val)
+print(new_head.next.next.next.next.val)
+print(new_head.next.next.next.next.next)
