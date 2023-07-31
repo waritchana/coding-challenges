f065162c8f19839add05f843098650118152f8a9
win-study <win-study@Air-2020.local>
2023-07-30 22:59:26 -0700

    Add input in ListNode object and print output :honeybee:

--- easy/83_remove_duplicates_from_sorted_linked_list.py
+++ easy/83_remove_duplicates_from_sorted_linked_list.py
@@ -2,10 +2,10 @@ from typing import Optional
 
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
@@ -21,6 +21,25 @@ class Solution:
         return head
 
 
-head = [1,1,2,3,3]
-#head = [1,1,2]
-Solution().deleteDuplicates(head)
+# 1, 1, 2, 3, 3
+head = ListNode(
+    val=1,
+    next=ListNode(
+        val=1,
+        next=ListNode(
+            val=2,
+            next=ListNode(
+                val=3,
+                next=ListNode(
+                    val=3,
+                    next=None
+                )
+            )
+        )
+    )
+)
+# Expect 1, 2, 3
+new_head = Solution().deleteDuplicates(head)
+print(new_head.val)
+print(new_head.next.val)
+print(new_head.next.next.val)
