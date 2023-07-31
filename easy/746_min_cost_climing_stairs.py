14f4c843af4ba373f8824ab0af069fe33040d867
win-study <win-study@Air-2020.local>
2023-07-30 22:35:30 -0700

    Add input and print output :honeybee:

--- easy/746_min_cost_climing_stairs.py
+++ easy/746_min_cost_climing_stairs.py
@@ -11,3 +11,9 @@ class Solution:
         for i in range(len(cost)-3, -1, -1):
             cost[i] += min(cost[i+1], cost[i+2])
         return min(cost[0], cost[1])
+
+
+#cost = [10,15,20]
+cost = [1,100,1,1,1,100,1,1,100,1]
+min_cost = Solution().minCostClimbingStairs(cost)
+print(f"Minimum cost to reach the top of the floor is {min_cost}")
